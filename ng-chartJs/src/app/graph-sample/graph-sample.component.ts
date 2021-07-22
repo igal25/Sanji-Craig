6fd91735513e3db84d0103ed1ae18810b0170c4b
igal25 <i.brodsky2001@gmail.com>
2021-07-22 11:02:06 +0300

    onClick event all over the canvas

--- ng-chartJs/src/app/graph-sample/graph-sample.component.ts
+++ ng-chartJs/src/app/graph-sample/graph-sample.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Chart, registerables,Point } from 'chart.js';
+import { Chart, registerables, Point, Interaction } from 'chart.js';
 import colorLib from '@kurkle/color';
-import {DateTime} from 'luxon';
+import { DateTime } from 'luxon';
 //import 'chartjs-adapter-luxon';
-import {valueOrDefault} from '../../../node_modules/chart.js/helpers/helpers.esm';
+import { valueOrDefault } from '../../../node_modules/chart.js/helpers/helpers.esm';
+import { Action } from 'rxjs/internal/scheduler/Action';
 
 @Component({
   selector: 'app-graph-sample',
@@ -14,31 +15,38 @@ export class GraphSampleComponent implements OnInit {
 
   chart: any;
   DATA_COUNT = 1000;
-  NUMBER_CFG = {count: this.DATA_COUNT, min: -180, max: 180};
+  NUMBER_CFG = { count: this.DATA_COUNT, min: -180, max: 180 };
 
 
   constructor() { }
-
+  
   ngOnInit(): void {
     this.chart = document.getElementById("scattered_chart");
     Chart.register(...registerables);
     this.loadChart();
-    setInterval( () => { 
+
+    setInterval(() => {
       this.chart.destroy();
       this.chart = document.getElementById("scattered_chart");
       Chart.register(...registerables);
-      this.updateChart(); },
-     3000
-    );
+      this.loadChart();
+    },
+      30000);
   }
+    func1(x:MouseEvent){
+        alert(`${x.x} and ${x.y}`);
+      }
+
 
   loadChart() {
     this.chart = new Chart(this.chart, {
-      type:'scatter',
+      type: 'scatter',
+      
       data: {
         datasets: [
           {
-            label: 'Dataset 1',
+            
+            label: 'Dataset',
             data: this.points(this.NUMBER_CFG),
             fill: false,
             borderColor: this.CHART_COLORS.red,
@@ -47,12 +55,17 @@ export class GraphSampleComponent implements OnInit {
         ]
       },
       options: {
+        events: ['click'],
         responsive: true,
         plugins: {
+          
+          tooltip: { mode: 'point',enabled:false, },
+          legend: {
+          },
           title: {
             display: true,
             text: 'Axis Center Positioning'
-          }
+          },
         },
         scales: {
           x: {
@@ -63,202 +76,172 @@ export class GraphSampleComponent implements OnInit {
             min: 0,
             max: 100,
           }
-        },
-         
+        }
       }
-    })
+    });
+    onclick=(x)=>this.func1(x);
   }
 
-  updateChart(){
-    this.chart = new Chart(this.chart,{
-      type:'scatter',
-      data: {
-        datasets: [
-          {
-            label: 'Dataset 1',
-            data: this.points(this.NUMBER_CFG),
-            fill: false,
-            borderColor: this.CHART_COLORS.red,
-            backgroundColor: this.transparentize(this.CHART_COLORS.red, 1),
-          },
-        ]
-      },
-      options: {
-        responsive: true,
-        plugins: {
-          title: {
-            display: true,
-            text: 'Axis Center Positioning'
-          }
-        },
-        scales: {
-          x: {
-            min: -180,
-            max: 180,
-          },
-          y: {
-            min: 0,
-            max: 100,
-          }
-        },
-         
-      }
-    })
-}
+
+
+
+
+
+    
   _seed = Date.now();
 
-  srand(seed:any) {
-      this._seed = seed;
+  srand(seed: any) {
+    this._seed = seed;
   }
 
-  rand(min:any, max:any) {
-  min = valueOrDefault(min, 0);
-  max = valueOrDefault(max, 0);
-  this._seed = (this._seed * 9301 + 49297) % 233280;
-  return min + (this._seed / 233280) * (max - min);
+  rand(min: any, max: any) {
+    min = valueOrDefault(min, 0);
+    max = valueOrDefault(max, 0);
+    this._seed = (this._seed * 9301 + 49297) % 233280;
+    return min + (this._seed / 233280) * (max - min);
   }
 
-  numbers(config:any) {
-  var cfg = config || {};
-  var min = valueOrDefault(cfg.min, 0);
-  var max = valueOrDefault(cfg.max, 100);
-  var from = valueOrDefault(cfg.from, []);
-  var count = valueOrDefault(cfg.count, 8);
-  var decimals = valueOrDefault(cfg.decimals, 8);
-  var continuity = valueOrDefault(cfg.continuity, 1);
-  var dfactor = Math.pow(10, decimals) || 0;
-  var data = [];
-  var i, value;
+  numbers(config: any) {
+    var cfg = config || {};
+    var min = valueOrDefault(cfg.min, 0);
+    var max = valueOrDefault(cfg.max, 100);
+    var from = valueOrDefault(cfg.from, []);
+    var count = valueOrDefault(cfg.count, 8);
+    var decimals = valueOrDefault(cfg.decimals, 8);
+    var continuity = valueOrDefault(cfg.continuity, 1);
+    var dfactor = Math.pow(10, decimals) || 0;
+    var data = [];
+    var i, value;
 
-  for (i = 0; i < count; ++i) {
+    for (i = 0; i < count; ++i) {
       value = (from[i] || 0) + this.rand(min, max);
-      if (this.rand(min,max) <= continuity) {
-      data.push(Math.round(dfactor * value) / dfactor);
+      if (this.rand(min, max) <= continuity) {
+        data.push(Math.round(dfactor * value) / dfactor);
       } else {
-      data.push(null);
+        data.push(null);
       }
+    }
+
+    return data;
   }
 
-  return data;
+  points(config: any) {
+    const xs = this.numbers(config);
+    const ys = this.numbers(config);
+    return xs.map((x, i) => ({ x, y: ys[i] }));
   }
 
-  points(config:any) {
-  const xs = this.numbers(config);
-  const ys = this.numbers(config);
-  return xs.map((x, i) => ({x, y: ys[i]}));
-  }
-
-  bubbles(config:any) {
-  return this.points(config).map(pt => {
+  bubbles(config: any) {
+    return this.points(config).map(pt => {
       pt = this.rand(config.rmin, config.rmax);
       return pt;
-  });
+    });
   }
 
-  labels(config:any) {
-  var cfg = config || {};
-  var min = cfg.min || 0;
-  var max = cfg.max || 100;
-  var count = cfg.count || 8;
-  var step = (max - min) / count;
-  var decimals = cfg.decimals || 8;
-  var dfactor = Math.pow(10, decimals) || 0;
-  var prefix = cfg.prefix || '';
-  var values = [];
-  var i;
+  labels(config: any) {
+    var cfg = config || {};
+    var min = cfg.min || 0;
+    var max = cfg.max || 100;
+    var count = cfg.count || 8;
+    var step = (max - min) / count;
+    var decimals = cfg.decimals || 8;
+    var dfactor = Math.pow(10, decimals) || 0;
+    var prefix = cfg.prefix || '';
+    var values = [];
+    var i;
 
-  for (i = min; i < max; i += step) {
+    for (i = min; i < max; i += step) {
       values.push(prefix + Math.round(dfactor * i) / dfactor);
-  }
+    }
 
-  return values;
+    return values;
   }
 
   readonly MONTHS = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December'
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
   ];
 
-  months(config:any) {
-  var cfg = config || {};
-  var count = cfg.count || 12;
-  var section = cfg.section;
-  var values = [];
-  var i, value;
+  months(config: any) {
+    var cfg = config || {};
+    var count = cfg.count || 12;
+    var section = cfg.section;
+    var values = [];
+    var i, value;
 
-  for (i = 0; i < count; ++i) {
+    for (i = 0; i < count; ++i) {
       value = this.MONTHS[Math.ceil(i) % 12];
       values.push(value.substring(0, section));
-  }
+    }
 
-  return values;
+    return values;
   }
 
   readonly COLORS = [
-  '#4dc9f6',
-  '#f67019',
-  '#f53794',
-  '#537bc4',
-  '#acc236',
-  '#166a8f',
-  '#00a950',
-  '#58595b',
-  '#8549ba'
+    '#4dc9f6',
+    '#f67019',
+    '#f53794',
+    '#537bc4',
+    '#acc236',
+    '#166a8f',
+    '#00a950',
+    '#58595b',
+    '#8549ba'
   ];
 
-   color(index:any) {
-  return this.COLORS[index % this.COLORS.length];
+  color(index: any) {
+    return this.COLORS[index % this.COLORS.length];
   }
 
-  transparentize(value:any, opacity:any) {
-  var alpha = opacity === undefined ? 0.5 : 1 - opacity;
-  return colorLib(value).alpha(alpha).rgbString();
+  transparentize(value: any, opacity: any) {
+    var alpha = opacity === undefined ? 0.5 : 1 - opacity;
+    return colorLib(value).alpha(alpha).rgbString();
   }
 
   readonly CHART_COLORS = {
-  red: 'rgb(255, 99, 132)',
-  orange: 'rgb(255, 159, 64)',
-  yellow: 'rgb(255, 205, 86)',
-  green: 'rgb(75, 192, 192)',
-  blue: 'rgb(54, 162, 235)',
-  purple: 'rgb(153, 102, 255)',
-  grey: 'rgb(201, 203, 207)'
+    red: 'rgb(255, 99, 132)',
+    orange: 'rgb(255, 159, 64)',
+    yellow: 'rgb(255, 205, 86)',
+    green: 'rgb(75, 192, 192)',
+    blue: 'rgb(54, 162, 235)',
+    purple: 'rgb(153, 102, 255)',
+    grey: 'rgb(201, 203, 207)'
   };
 
   readonly NAMED_COLORS = [
-  this.CHART_COLORS.red,
-  this.CHART_COLORS.orange,
-  this.CHART_COLORS.yellow,
-  this.CHART_COLORS.green,
-  this.CHART_COLORS.blue,
-  this.CHART_COLORS.purple,
-  this.CHART_COLORS.grey,
+    this.CHART_COLORS.red,
+    this.CHART_COLORS.orange,
+    this.CHART_COLORS.yellow,
+    this.CHART_COLORS.green,
+    this.CHART_COLORS.blue,
+    this.CHART_COLORS.purple,
+    this.CHART_COLORS.grey,
   ];
 
-  namedColor(index:any) {
-  return this.NAMED_COLORS[index % this.NAMED_COLORS.length];
+  namedColor(index: any) {
+    return this.NAMED_COLORS[index % this.NAMED_COLORS.length];
   }
 
-  newDate(days:any) {
-  return DateTime.now().plus({days}).toJSDate();
+  newDate(days: any) {
+    return DateTime.now().plus({ days }).toJSDate();
   }
 
-  newDateString(days:any) {
-  return DateTime.now().plus({days}).toISO();
+  newDateString(days: any) {
+    return DateTime.now().plus({ days }).toISO();
   }
 
-  parseISODate(str:any) {
-  return DateTime.fromISO(str);
+  parseISODate(str: any) {
+    return DateTime.fromISO(str);
   }
 
 
